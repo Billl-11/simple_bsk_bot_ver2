1b759cdb9eb5bd6d2f892b5be3093ad61001c0d2
Maulaya Radhibilla <abilradhibilla@gmail.com>
2024-08-01 11:20:45 +0700

    add another spinner loading

--- main.py
+++ main.py
@@ -62,10 +62,11 @@ if prompt:= st.chat_input("What is up?"):
             st.toast('⚙️ A function was called!..')
             print('tool used')
             for tool_call in tool_calls:
-                tool_id, tool_name, tool_arguments, tool_params = utils.extract_tool_details(tool_call)
-                utils.add_tool_detail(st.session_state.chat_history, tool_id, tool_name, tool_arguments, tool_params)
-                tool_call_result = eval(f"ct.{tool_name}")(**tool_params)
-                utils.add_tool_response(st.session_state.chat_history, tool_id, tool_name, tool_call_result)
+                with st.spinner('Running function...'):
+                    tool_id, tool_name, tool_arguments, tool_params = utils.extract_tool_details(tool_call)
+                    utils.add_tool_detail(st.session_state.chat_history, tool_id, tool_name, tool_arguments, tool_params)
+                    tool_call_result = eval(f"ct.{tool_name}")(**tool_params)
+                    utils.add_tool_response(st.session_state.chat_history, tool_id, tool_name, tool_call_result)
             
             if model_option == "OpenAI (API)":
                 with st.spinner('Thinking...'):
